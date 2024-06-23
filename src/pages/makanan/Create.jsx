d8c58d7ac1af0b9ee8ee8fb9ed3cac5e99ad8e51
muhammadadid <muhammadadid20@gmail.com>
2024-06-23 15:51:25 +0700

    add Axios

--- src/pages/makanan/Create.jsx
+++ src/pages/makanan/Create.jsx
@@ -1,6 +1,6 @@
 import NavBar from "@/components/NavBar";
 import FoodForm from "@/components/FoodForm";
-
+import axios from "axios";
 export default function Create() {
   return (
     <div className="min-h-screen bg-slate-300">
